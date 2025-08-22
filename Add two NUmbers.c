ebba6e1e84f72afcf952913ecfa58207caa5716c
hvivid007-arch <hvivid007@gmail.com>
2025-08-22 11:31:02 +0530

    Add files via upload

---
+++ Add two NUmbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int a, b, sum;
+   printf("Give two numers:\n");
+   scanf("%d %d",&a, &b);
+   sum=a + b;
+   printf("sum of given numbers is:%d\n",sum);
+   return 0;
+    }

90c617d2a924b0ded89bd3b1d898ed28fe4f6d78
hvivid007-arch <hvivid007@gmail.com>
2025-08-23 10:26:56 +0530

    Add files via upload

---
+++ convert dollars into pounds.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+float dollars,pounds;
+printf("enter amount in dollars:\n");
+scanf("%f",&dollars);
+pounds=(dollars*48)/70;
+printf("pounds=%.2f\n",pounds);
+retuen 0;
+}
